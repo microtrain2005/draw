3cf52f59408333c721188b44f927f6e736932e1b
chad svastisalee <swasgrafx@gmail.com>
2020-06-01 13:32:00 -0500

    Draw Random Rectangle

---
+++ src/js/main.js
@@ -0,0 +1,126 @@
+var draw = (function() {
+
+    //Get the height and width of the main we will use this set canvas to the full
+    //size of the main element.
+    var mWidth = document.querySelector('main').offsetWidth,
+      mHeight = document.querySelector('main').offsetHeight,
+  
+      //Create the canvas
+      canvas = document.createElement("canvas"),
+  
+      //Create the context
+      ctx = canvas.getContext("2d"),
+  
+      //Create the initial bounding rectangle
+      rect = canvas.getBoundingClientRect(),
+  
+      //current x,y position
+      x=0,
+      y=0;
+  
+    //starting x,y
+    x1=0,
+    y1=0,
+
+    //ending x,y
+    x2=0,
+    y2=0;
+
+    //What shape are we drawing?
+    shape='';
+
+    return {
+      //Set the x,y coords based on current event data
+      setXY: function(evt) {
+        x = (evt.clientX - rect.left) - canvas.offsetLeft;
+        y = (evt.clientY - rect.top) - canvas.offsetTop;
+      },
+  
+      //Write the x,y coods to the target div
+      writeXY: function() {
+        document.getElementById('trackX').innerHTML = 'X: ' + x;
+        document.getElementById('trackY').innerHTML = 'Y: ' + y;
+      },
+  
+      setStart: function() {
+        x1=x;
+        y1=y;
+      },
+      
+      setEnd: function() {
+        x2=x;
+        y2=y;
+      },
+      
+      //Sets the shape to be drawn
+      setShape: function(shp) {
+        shape = shp;
+      },
+
+      draw: function() {
+        ctx.restore();
+        if(shape==='rectangle')
+        {
+          this.drawRect();
+        } else {
+          alert('Please choose a shape');
+        }
+        ctx.save();
+      
+        if(shape==='rectangle')
+        {
+          this.drawRect();
+        } else {
+          alert('Please choose a shape');
+        }
+      },
+      
+      //Draw a rectangle
+      drawRect: function(x,y,h,w) {
+
+        //Start by using random fill colors.
+        ctx.fillStyle = '#'+Math.floor(Math.random()*16777215).toString(16);
+      
+        ctx.fillRect (x1,y1,(x2-x1),(y2-y1));
+      
+      },
+        
+      getCanvas: function(){
+        return canvas;
+      },
+  
+      //Initialize the object, this must be called before anything else
+      init: function() {
+        canvas.width = mWidth;
+        canvas.height = mHeight;
+        document.querySelector('main').appendChild(canvas);
+  
+      }
+    };
+  
+  })();
+  
+  //Initialize draw
+  draw.init();
+  
+  //Add a mousemove listener to the canvas
+  //When the mouse reports a change of position use the event data to
+  //set and report the x,y position on the mouse.
+  draw.getCanvas().addEventListener('mousemove', function(evt) {
+    draw.setXY(evt);
+    draw.writeXY();
+  }, false);
+  
+  //Set the starting position
+  draw.getCanvas().addEventListener('mousedown', function() {
+    draw.setStart();
+  }, false);
+
+  draw.getCanvas().addEventListener('mouseup', function() {
+    draw.setEnd();
+    draw.draw();
+  }, false);
+    
+  document.getElementById('btnRect').addEventListener('click',function(){
+    draw.setShape('rectangle');
+}, false);
