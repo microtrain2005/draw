bb16c6aa5b90832fd9f4d106761cd1e42fff1a7a
chad svastisalee <swasgrafx@gmail.com>
2020-06-01 14:32:48 -0500

    Added Random Path

--- src/js/main.js
+++ src/js/main.js
@@ -26,16 +26,28 @@ var draw = (function() {
     x2=0,
     y2=0;
 
+    //Tracks the last x,y state
+    lx = false,
+    ly = false,
+  
     //What shape are we drawing?
     shape='';
 
+    isDrawing=false;
+
     return {
-      //Set the x,y coords based on current event data
-      setXY: function(evt) {
-        x = (evt.clientX - rect.left) - canvas.offsetLeft;
-        y = (evt.clientY - rect.top) - canvas.offsetTop;
-      },
-  
+        //Set the x,y coords based on current event data
+        setXY: function(evt) {
+
+            //Track the last x,y position before setting the current position.
+            lx=x;
+            ly=y;
+        
+            //Set the current x,y position
+            x = (evt.clientX - rect.left) - canvas.offsetLeft;
+            y = (evt.clientY - rect.top) - canvas.offsetTop;
+        },
+    
       //Write the x,y coods to the target div
       writeXY: function() {
         document.getElementById('trackX').innerHTML = 'X: ' + x;
@@ -51,12 +63,22 @@ var draw = (function() {
         x2=x;
         y2=y;
       },
+
+
       
       //Sets the shape to be drawn
       setShape: function(shp) {
         shape = shp;
       },
 
+      setIsDrawing: function(bool) {
+        isDrawing = bool;
+      },
+      
+      getIsDrawing: function() {
+        return isDrawing;
+      },
+      
       draw: function() {
         ctx.restore();
         if(shape==='rectangle')
@@ -66,7 +88,9 @@ var draw = (function() {
           this.drawLine();
         } else if(shape==='circle') {
           this.drawCircle();
-        } else {
+        } else if(shape==='path') {
+          this.drawPath();
+          } else {
           alert('Please choose a shape');
         }
         ctx.save();
@@ -107,8 +131,21 @@ var draw = (function() {
             ctx.stroke();
             ctx.fill();
         },
-  
-        
+
+        //Draw a path
+        drawPath: function() {
+            //Start by using random fill colors.
+            ctx.strokeStyle = '#'+Math.floor(Math.random()*16777215).toString(16);
+            ctx.beginPath();
+            ctx.moveTo(lx, ly);
+            ctx.lineTo(x, y);
+            ctx.stroke();
+        },
+                    
+    getShape: function() {
+    return shape;
+    },
+                  
       getCanvas: function(){
         return canvas;
       },
@@ -133,16 +170,21 @@ var draw = (function() {
   draw.getCanvas().addEventListener('mousemove', function(evt) {
     draw.setXY(evt);
     draw.writeXY();
+    if(draw.getShape()=='path' && draw.getIsDrawing()===true) {
+      draw.draw();
+    }
   }, false);
-  
+      
   //Set the starting position
   draw.getCanvas().addEventListener('mousedown', function() {
     draw.setStart();
+    draw.setIsDrawing(true);
   }, false);
 
   draw.getCanvas().addEventListener('mouseup', function() {
     draw.setEnd();
     draw.draw();
+    draw.setIsDrawing(false);
   }, false);
     
   document.getElementById('btnRect').addEventListener('click',function(){
@@ -155,6 +197,10 @@ document.getElementById('btnLine').addEventListener('click',function(){
 
 document.getElementById('btnCircle').addEventListener('click',function(){
     draw.setShape('circle');
+}, false);
+
+document.getElementById('btnPath').addEventListener('click',function(){
+    draw.setShape('path');
 }, false);
 
 
